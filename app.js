19f87a3386db25cc4e2f076ea4c59c4dc7db877f
Moses Femi Smauel <mosesfemi10@gmail.com>
2025-02-05 13:44:51 +0100

    setting up server

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+    
+});
+
+
+
