09c563fb852e1fdd2f4464bed2cdaa7f36b3ed35
Moses Femi Smauel <mosesfemi10@gmail.com>
2025-02-06 09:10:05 +0100

    imporrt route to app

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
+const route = require('./routes/tasks');
+
+//routes
+app.get('/', (req,res) => {
+    res.send('Task Manager App');
+});
+
+//app.get('api/v1/tasks')   - get all task
+//app.post('api/v1/tasks')  - create a new task
+//app.get('api/v1/tasks/:id') - get single task
+//app.patch('api/v1/tasks/:id') - update task
+//app.delete('api/v1/tasks/:id') - delete task
+
+
+
+
 
 const port = 3000;
-
 
 
 app.listen(port, () => {
