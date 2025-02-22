071f96cfed5e640937e93149c5809106d5b53ee7
Moses Femi Smauel <mosesfemi10@gmail.com>
2025-02-22 08:14:01 +0100

    new commit

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const app = express();
 const routes = require('./routes/tasks');
+const app = express();
+const connectDB = require('./db/connect');
+require('dotenv').config();
+
 
 //middleware
 app.use(express.json());
@@ -11,23 +14,24 @@ app.get('/', (req,res) => {
 });
 
 app.use('/api/v1/tasks', routes);
-//app.get('api/v1/tasks')   - get all task
-//app.post('api/v1/tasks')  - create a new task
-//app.get('api/v1/tasks/:id') - get single task
-//app.patch('api/v1/tasks/:id') - update task
-//app.delete('api/v1/tasks/:id') - delete task
-
-
 
 
 
 const port = 3000;
 
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)  
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}.............`);
+            
+        });
+    } catch (error) {
+        console.log(error);
+        
+    }
+};
 
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-    
-});
-
+start();
 
 
--- routes/tasks.js
+++ routes/tasks.js
@@ -6,12 +6,6 @@ router.route('/').get(getAllTasks).post(createTasks);
 router.route('/:id').get(getTask).patch(updateTasks).delete(deleteTasks);
 
 
-//app.get('api/v1/tasks')   - get all task
-//app.post('api/v1/tasks')  - create a new task
-//app.get('api/v1/tasks/:id') - get single task
-//app.patch('api/v1/tasks/:id') - update task
-//app.delete('api/v1/tasks/:id') - delete task
-
 
 
 
